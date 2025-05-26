95e0be63e9e265bc3559e87318b1c07870ee8ad0
kerosene2562 <ipzk241_roa@student.ztu.edu.ua>
2025-05-26 19:08:55 +0300

    firstTask

---
+++ NewsBlazor/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewsBlazor.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        public ICollection<News> News { get; set; }
+    }
+}
---
+++ NewsBlazor/Models/News.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NewsBlazor.Models
+{
+    public class News
+    {
+        [JsonPropertyName("newsID")]
+        public long? NewsID { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("publicationDate")]
+        public DateTime PublicationDate { get; set; }
+
+        [JsonPropertyName("imgDirUrl")]
+        public string ImgDirUrl { get; set; }
+
+        [JsonPropertyName("categoryID")]
+        public int CategoryID { get; set; }
+    }
+
+
+}
